9ac3d21ce39f9cd965f248a6f17c53b1e0993d80
Calvin Liang <me@calvin.sh>
2023-11-02 14:58:32 -0700

    feat: add github pages workflow

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://queercoded.ca/",
+  // site: "https://queercoded.ca/",
+  site: "https://QueerCodedUBC.github.io",
   integrations: [vue(), mdx()],
 });

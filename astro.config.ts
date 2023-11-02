d98491fd6a8f587e3de3d77ccfc5455190b09d35
Calvin Liang <me@calvin.sh>
2023-11-02 16:09:33 -0700

    fix: add astro config site fallback

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://queercoded.ca/",
-  site: process.env.URL,
+  site: process.env.URL ?? "https://queercoded.ca/",
   integrations: [vue(), mdx()],
 });

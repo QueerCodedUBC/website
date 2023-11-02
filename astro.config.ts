29d6328ccb6c105852ee441c1314079209980ddc
Calvin Liang <me@calvin.sh>
2023-11-02 15:53:19 -0700

    feat: change astro site

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   // site: "https://queercoded.ca/",
-  site: "https://QueerCodedUBC.github.io",
+  site: process.env.URL,
   integrations: [vue(), mdx()],
 });
